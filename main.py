4ff428de912913430bdf171429e7013b4b13a2fe
Lina <liroingo@gmail.com>
2024-11-29 22:58:44 -0500

    Implement basic endpoint accross all entities

--- main.py
+++ main.py
@@ -26,9 +26,14 @@ class Department(BaseModel):
 class HiredEmployee(BaseModel):
     id: int
     name: str
-    #datetime: datetime
+    datetime: datetime.datetime
     department_id: int
     job_id: int
+
+class Rawdata(BaseModel):
+    job: Optional[List[Job]] = []
+    department: Optional[List[Department]] = []
+    hired_employee: Optional[List[HiredEmployee]] = []
 
 #Define a basic route
 @app.get("/")
@@ -36,8 +41,22 @@ def read_root():
     return {"message": "Welcome to FastAPI"}
 
 @app.post("/receive_data")
-async def receive_data(job: Job):
-    # Create a new user with an auto-incremented ID
-    id = len(jobs_db) + 1
-    new_job = {"id": id, **job.dict()}
-    return new_job
+async def receive_data(data: Rawdata):
+    try:
+        # Insert data for jobs table
+        if data.job:
+            jobs_entries = [Job(**item.dict()) for item in data.job]
+
+        # Insert data for departments table
+        if data.department:
+            department_entries = [Department(**item.dict()) for item in data.department]
+
+        # Insert data for hired_employees table
+        if data.hired_employee:
+            hired_employee_entries = [HiredEmployee(**item.dict()) for item in data.hired_employee]
+
+        return jobs_entries, department_entries, hired_employee_entries
+        #return {"message": "Data received and saved successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error while saving data: " + str(e))
