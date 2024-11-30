8dd8cba63f8f24284aebb048b844c94fde8004d9
Lina <liroingo@gmail.com>
2024-11-29 22:04:54 -0500

    adds post function

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+import datetime as datetime
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 # Create an instance of the FastAPI app
 app = FastAPI()
+
+# In-memory "database"
+jobs_db = []
+departments_db = []
+hired_employees_db = []
+
+# --- pydantic models --- #
+# Model for Job table
+class Job(BaseModel):
+    id: int
+    job: str
+
+# Model for Department table
+class Department(BaseModel):
+    id: int
+    department: str
+
+# Model for Hired Employees table
+class HiredEmployee(BaseModel):
+    id: int
+    name: str
+    #datetime: datetime
+    department_id: int
+    job_id: int
 
 #Define a basic route
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI"}
 
-@app.get("/items/{item_id")
-def read_item(item_id: int, q: str = None):
-    return {"item_id":item_id, "query": q}
+@app.post("/receive_data")
+async def receive_data(job: Job):
+    # Create a new user with an auto-incremented ID
+    id = len(jobs_db) + 1
+    new_job = {"id": id, **job.dict()}
+    return new_job
